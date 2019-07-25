57521d398a56f4e69bf2ba6fee0326b9bbbacd78
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-07-25 14:57:27 -0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -18,6 +18,20 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Image.asset('images/dice1.png'),
+            ),
+            Expanded(
+              child: Image.asset('images/dice1.png'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
